cd429a6e7aac875c85150265159b5511e99e0db4
Jordi Josue Cuevas <cuevasj@zippyttech.com>
2018-07-31 15:40:02 -0400

    Added dashboard grid

--- src/app/material.ts
+++ src/app/material.ts
@@ -7,13 +7,30 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-
+import { MatGridListModule } from '@angular/material/grid-list';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [MatMenuModule, MatListModule, MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule ],
-    exports: [MatMenuModule, MatListModule, MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule ],
+  imports: [
+    MatGridListModule,
+    MatMenuModule,
+    MatListModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  exports: [
+    MatGridListModule,
+    MatMenuModule,
+    MatListModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
+  ]
 })
-
 export class MaterialModule {}
 

4283358e3173740f49d8e06e5abbe210a7976976
Jordi Josue Cuevas <cuevasj@zippyttech.com>
2018-08-01 09:55:23 -0400

    Added Dashboard modifications

--- src/app/modules/dashboard/topbar/topbar.component.ts
+++ src/app/modules/dashboard/topbar/topbar.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
- import { MatSidenav } from '@angular/material';
-
+import { MatSidenav } from '@angular/material';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
-
 export class TopbarComponent implements OnInit {
   // @ViewChild('right') sidenav: MatSidenav;
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
   ngOnInit() {
-     // this.sidenav.open();
+    // this.sidenav.open();
   }
 }

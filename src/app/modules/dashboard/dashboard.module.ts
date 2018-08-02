8e807d0b755543913467b5e147f36315718235b2
Jordi Josue Cuevas <cuevasj@zippyttech.com>
2018-08-02 09:49:32 -0400

    Added fixes to dashboard interface

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,12 +2,12 @@ import { NgModule, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopbarComponent } from './topbar/topbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { MaterialModule } from '../../material';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { RouterModule } from '@angular/router';
-import { Title } from '@angular/platform-browser';
-import { Router } from '@angular/router';
 import { InitComponent } from './init/init.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { MaterialModule } from '../../material';
+import { RouterModule } from '@angular/router';
+
  @NgModule({
   imports: [
     CommonModule,
@@ -17,7 +17,8 @@ import { InitComponent } from './init/init.component';
   exports: [
     TopbarComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    InitComponent
   ],
   declarations: [TopbarComponent, FooterComponent, DashboardComponent, InitComponent]
 })

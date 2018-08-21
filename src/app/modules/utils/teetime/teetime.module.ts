a145df44556cc25b6d5d90ba1c355506a1c27cc3
Jordi Cuevas <jordicuevas@gmail.com>
2018-08-21 07:57:22 -0400

    chnages in reservar html

--- src/app/modules/utils/teetime/teetime.module.ts
+++ src/app/modules/utils/teetime/teetime.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeetimeComponent } from './teetime/teetime.component';
+import { MaterialModule } from '../../../material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports: [ TeetimeComponent ],
   declarations: [ TeetimeComponent ]
